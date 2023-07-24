4adecbdd21152207cb6db72989d28937ad85e477
Xikaro <0regon.end@gmail.com>
2023-07-24 23:42:29 +0500

    Update CreativeTabsTFC.java

--- src/main/java/net/dries007/tfc/objects/CreativeTabsTFC.java
+++ src/main/java/net/dries007/tfc/objects/CreativeTabsTFC.java
@@ -5,8 +5,8 @@
 
 package net.dries007.tfc.objects;
 
-import javax.annotation.Nonnull;
-
+import net.dries007.tfc.TerraFirmaCraft;
+import net.dries007.tfc.api.capability.food.CapabilityFood;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -15,23 +15,24 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import net.dries007.tfc.TerraFirmaCraft;
-import net.dries007.tfc.api.capability.food.CapabilityFood;
+import javax.annotation.Nonnull;
 
 import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
 
-public final class CreativeTabsTFC
-{
-    public static final CreativeTabs CT_ROCK_BLOCKS = new TFCCreativeTab("rock.blocks", "tfc:smooth/granite");
-    public static final CreativeTabs CT_ROCK_ITEMS = new TFCCreativeTab("rock.items", "tfc:ore/tetrahedrite");
-    public static final CreativeTabs CT_WOOD = new TFCCreativeTab("wood", "tfc:wood/log/pine");
-    public static final CreativeTabs CT_DECORATIONS = new TFCCreativeTab("decorations", "tfc:wall/cobble/granite");
-    public static final CreativeTabs CT_METAL = new TFCCreativeTab("metal", "tfc:metal/ingot/bronze");
-    public static final CreativeTabs CT_GEMS = new TFCCreativeTab("gems", "tfc:gem/diamond");
-    public static final CreativeTabs CT_POTTERY = new TFCCreativeTab("pottery", "tfc:ceramics/fired/mold/ingot");
-    public static final CreativeTabs CT_FOOD = new TFCCreativeTab("food", "tfc:food/green_apple");
-    public static final CreativeTabs CT_MISC = new TFCCreativeTab("misc", "tfc:wand");
-    public static final CreativeTabs CT_FLORA = new TFCCreativeTab("flora", "tfc:plants/goldenrod");
+public final class CreativeTabsTFC {
+
+    public static final CreativeTabs ROCK_STUFFS = new TFCCreativeTab("rock", "tfc:ordinary/raw/blueschist");
+    public static final CreativeTabs EARTH = new TFCCreativeTab("earth", "tfc:ordinary/grass/silt");
+
+
+    public static final CreativeTabs WOOD = new TFCCreativeTab("wood", "tfc:wood/log/pine");
+    public static final CreativeTabs DECORATIONS = new TFCCreativeTab("decorations", "tfc:chiseled/brick/blueschist");
+    public static final CreativeTabs METAL = new TFCCreativeTab("metal", "tfc:metal/ingot/bronze");
+    public static final CreativeTabs GEMS = new TFCCreativeTab("gems", "tfc:gem/diamond");
+    public static final CreativeTabs POTTERY = new TFCCreativeTab("pottery", "tfc:ceramics/fired/mold/ingot");
+    public static final CreativeTabs FOOD = new TFCCreativeTab("food", "tfc:food/green_apple");
+    public static final CreativeTabs MISC = new TFCCreativeTab("misc", "tfc:wand");
+    public static final CreativeTabs FLORA = new TFCCreativeTab("flora", "tfc:plants/goldenrod");
 
     private static class TFCCreativeTab extends CreativeTabs
     {

5c5543801f35816eafb61473494d799dfc0ae4c0
Xikaro <0regon.end@gmail.com>
2023-07-29 16:46:58 +0500

    Update WorldGenSpeleothem.java

--- src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenSpeleothem.java
+++ src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenSpeleothem.java
@@ -22,97 +22,97 @@ import static net.dries007.tfc.objects.blocks.rock.BlockRock.getBlockRockMap;
 
 public class WorldGenSpeleothem implements IWorldGenerator {
 
-    @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+	@Override
+	public void generate (Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 
-        int x = chunkX * 16 + 8;
-        int z = chunkZ * 16 + 8;
+		int x = chunkX * 16 + 8;
+		int z = chunkZ * 16 + 8;
 
-        int spread = 10;
-        int tries = 60;
-        int innerSpread = 6;
-        int innerTries = 12;
-        int upperBound = 55;
-        int offset = 6;
+		int spread = 10;
+		int tries = 60;
+		int innerSpread = 6;
+		int innerTries = 12;
+		int upperBound = 150;  // Максимальная высота генерации
+		int offset = 6;
 
-        if (upperBound > 0)
-            for (int i = 0; i < tries; i++) {
-                BlockPos target = new BlockPos(x + random.nextInt(spread), random.nextInt(upperBound) + offset, z + random.nextInt(spread));
-                if (placeSpeleothemCluster(random, world, target, innerSpread, innerTries))
-                    i++;
-            }
-    }
+		if (upperBound > 0)
+			for (int i = 0; i < tries; i++) {
+				BlockPos target = new BlockPos(x + random.nextInt(spread), random.nextInt(upperBound) + offset, z + random.nextInt(spread));
+				if (placeSpeleothemCluster(random, world, target, innerSpread, innerTries))
+					i++;
+			}
+	}
 
-    private boolean placeSpeleothemCluster(Random random, World world, BlockPos pos, int spread, int tries) {
-        if (!findAndPlaceSpeleothem(random, world, pos))
-            return false;
+	private boolean placeSpeleothemCluster (Random random, World world, BlockPos pos, int spread, int tries) {
+		if (!findAndPlaceSpeleothem(random, world, pos))
+			return false;
 
-        for (int i = 0; i < tries; i++) {
-            BlockPos target = pos.add(random.nextInt(spread * 2 + 1) - spread, random.nextInt(spread + 1) - spread, random.nextInt(spread * 2 + 1) - spread);
-            findAndPlaceSpeleothem(random, world, target);
-        }
+		for (int i = 0; i < tries; i++) {
+			BlockPos target = pos.add(random.nextInt(spread * 2 + 1) - spread, random.nextInt(spread + 1) - spread, random.nextInt(spread * 2 + 1) - spread);
+			findAndPlaceSpeleothem(random, world, target);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private boolean findAndPlaceSpeleothem(Random random, World world, BlockPos pos) {
-        if (!world.isAirBlock(pos))
-            return false;
+	private boolean findAndPlaceSpeleothem (Random random, World world, BlockPos pos) {
+		if (!world.isAirBlock(pos))
+			return false;
 
-        int off = world.provider.isNether() ? -1000 : 0;
-        boolean up = random.nextBoolean();
-        EnumFacing diff = (up ? EnumFacing.UP : EnumFacing.DOWN);
+		int off = world.provider.isNether() ? -1000 : 0;
+		boolean up = random.nextBoolean();
+		EnumFacing diff = (up ? EnumFacing.UP : EnumFacing.DOWN);
 
-        if (!up && world.canBlockSeeSky(pos))
-            return false;
+		if (!up && world.canBlockSeeSky(pos))
+			return false;
 
-        IBlockState stateAt;
-        do {
-            pos = pos.offset(diff);
-            stateAt = world.getBlockState(pos);
-            off++;
-        } while (pos.getY() > 4 && pos.getY() < 200 && !stateAt.isFullBlock() && off < 10);
+		IBlockState stateAt;
+		do {
+			pos = pos.offset(diff);
+			stateAt = world.getBlockState(pos);
+			off++;
+		} while (pos.getY() > 4 && pos.getY() < 200 && !stateAt.isFullBlock() && off < 10);
 
-        Block type = getSpeleothemType(stateAt);
-        placeSpeleothem(random, world, pos, type, !up);
+		Block type = getSpeleothemType(stateAt);
+		placeSpeleothem(random, world, pos, type, !up);
 
-        return true;
-    }
+		return true;
+	}
 
-    private void placeSpeleothem(Random random, World world, BlockPos pos, Block type, boolean up) {
-        if (type == null)
-            return;
+	private void placeSpeleothem (Random random, World world, BlockPos pos, Block type, boolean up) {
+		if (type == null)
+			return;
 
-        EnumFacing diff = up ? EnumFacing.UP : EnumFacing.DOWN;
-        int size = random.nextInt(3) == 0 ? 2 : 3;
-        if (!up && random.nextInt(20) == 0)
-            size = 1;
+		EnumFacing diff = up ? EnumFacing.UP : EnumFacing.DOWN;
+		int size = random.nextInt(3) == 0 ? 2 : 3;
+		if (!up && random.nextInt(20) == 0)
+			size = 1;
 
-        for (int i = 0; i < size; i++) {
-            pos = pos.offset(diff);
-            if (!world.isAirBlock(pos))
-                return;
-            if (type instanceof IRockTypeBlock) {
-                BlockRockSpeleothem.EnumSize sizeType = BlockRockSpeleothem.EnumSize.values()[size - i - 1];
-                IBlockState targetBlock = getBlockRockMap(ORDINARY, SPELEOTHEM, ((IRockTypeBlock) type).getRockType()).getDefaultState().withProperty(BlockRockSpeleothem.SIZE, sizeType);
-                world.setBlockState(pos, targetBlock);
-            }
+		for (int i = 0; i < size; i++) {
+			pos = pos.offset(diff);
+			if (!world.isAirBlock(pos))
+				return;
+			if (type instanceof IRockTypeBlock) {
+				BlockRockSpeleothem.EnumSize sizeType = BlockRockSpeleothem.EnumSize.values()[size - i - 1];
+				IBlockState targetBlock = getBlockRockMap(ORDINARY, SPELEOTHEM, ((IRockTypeBlock) type).getRockType()).getDefaultState().withProperty(BlockRockSpeleothem.SIZE, sizeType);
+				world.setBlockState(pos, targetBlock);
+			}
 
 
-        }
-    }
+		}
+	}
 
-    @SuppressWarnings("incomplete-switch")
-    private Block getSpeleothemType(IBlockState state) {
-        Block block = state.getBlock();
-        for (RockType rockType : RockType.values()) {
-            if (getBlockRockMap(ORDINARY, RAW, rockType) == block) {
-                return getBlockRockMap(ORDINARY, RAW, rockType);
-            }
-        }
+	@SuppressWarnings("incomplete-switch")
+	private Block getSpeleothemType (IBlockState state) {
+		Block block = state.getBlock();
+		for (RockType rockType : RockType.values()) {
+			if (getBlockRockMap(ORDINARY, RAW, rockType) == block) {
+				return getBlockRockMap(ORDINARY, RAW, rockType);
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
 
 }

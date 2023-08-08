bedfd8c6eeab01f02d6ece73c7822bbff4961d0f
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-08-08 11:13:45 +0700

    dead

--- src/main/java/net/dries007/tfc/api/types/rock/block/variant/RockVariants.java
+++ src/main/java/net/dries007/tfc/api/types/rock/block/variant/RockVariants.java
@@ -1,13 +1,46 @@
 package net.dries007.tfc.api.types.rock.block.variant;
 
+import net.dries007.tfc.api.types.rock.block.type.RockType;
+
 public class RockVariants {
     public static RockVariant RAW;
+
     public static RockVariant COBBLE;
+    public static RockVariant COBBLE_MOSSY;
+
     public static RockVariant SMOOTH;
+
     public static RockVariant BRICK;
+    public static RockVariant BRICK_MOSSY;
     public static RockVariant BRICK_CRACKED;
     public static RockVariant BRICK_CHISELED;
 
     public static RockVariant GRAVEL;
     public static RockVariant SAND;
+
+    public static RockVariant STAIRS_RAW;
+    public static RockVariant STAIRS_COBBLE;
+    public static RockVariant STAIRS_SMOOTH;
+    public static RockVariant STAIRS_BRICK;
+
+    public static RockVariant SLAB_DOUBLE_RAW;
+    public static RockVariant SLAB_DOUBLE_COBBLE;
+    public static RockVariant SLAB_DOUBLE_SMOOTH;
+    public static RockVariant SLAB_DOUBLE_BRICK;
+
+    public static RockVariant SLAB_RAW;
+    public static RockVariant SLAB_COBBLE;
+    public static RockVariant SLAB_SMOOTH;
+    public static RockVariant SLAB_BRICK;
+
+    public static RockVariant WALL_RAW;
+    public static RockVariant WALL_COBBLE;
+    public static RockVariant WALL_SMOOTH;
+    public static RockVariant WALL_BRICK;
+
+    public static RockVariant LOOSE;
+    public static RockVariant SPELEOTHEM;
+    public static RockVariant BUTTON;
+    public static RockVariant PRESSURE_PLATE;
+    public static RockVariant ANVIL;
 }

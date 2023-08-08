7e1155c7b57ddf2d084c50dd69fcc8e75a69645f
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-08-08 13:07:08 +0700

    refactor

--- src/main/java/net/dries007/tfc/api/types/wood/variant/WoodBlockVariantHandler.java
+++ src/main/java/net/dries007/tfc/api/types/wood/variant/WoodBlockVariantHandler.java
@@ -1,33 +1,35 @@
 package net.dries007.tfc.api.types.wood.variant;
 
+import static net.dries007.tfc.api.types.wood.variant.WoodBlockVariants.*;
+
 public class WoodBlockVariantHandler {
     public static void init() {
 
-        WoodBlockVariants.LOG = new WoodBlockVariant("log");
-        WoodBlockVariants.STRIPPED_LOG = new WoodBlockVariant("stripped_log");
-        WoodBlockVariants.WOOD = new WoodBlockVariant("wood");
-        WoodBlockVariants.STRIPPED_WOOD = new WoodBlockVariant("stripped_wood");
-        WoodBlockVariants.LEAVES = new WoodBlockVariant("leaves");
-        WoodBlockVariants.PLANKS = new WoodBlockVariant("planks");
-        WoodBlockVariants.SAPLING = new WoodBlockVariant("sapling");
-        WoodBlockVariants.POTTED_SAPLING = new WoodBlockVariant("potted_sapling");
-        WoodBlockVariants.BOOKSHELF = new WoodBlockVariant("bookshelf");
-        WoodBlockVariants.DOOR = new WoodBlockVariant("door");
-        WoodBlockVariants.TRAPDOOR = new WoodBlockVariant("trapdoor");
-        WoodBlockVariants.FENCE = new WoodBlockVariant("fence");
-        WoodBlockVariants.LOG_FENCE = new WoodBlockVariant("log_fence");
-        WoodBlockVariants.FENCE_GATE = new WoodBlockVariant("fence_gate");
-        WoodBlockVariants.BUTTON = new WoodBlockVariant("button");
-        WoodBlockVariants.PRESSURE_PLATE = new WoodBlockVariant("pressure_plate");
-        WoodBlockVariants.SLAB_DOUBLE = new WoodBlockVariant("slab_double");
-        WoodBlockVariants.SLAB = new WoodBlockVariant("slab");
-        WoodBlockVariants.STAIRS = new WoodBlockVariant("stairs");
-        WoodBlockVariants.TOOL_RACK = new WoodBlockVariant("tool_rack");
-        WoodBlockVariants.SUPPORT = new WoodBlockVariant("support");
-        WoodBlockVariants.WORKBENCH = new WoodBlockVariant("workbench");
-        WoodBlockVariants.CHEST_TRAP = new WoodBlockVariant("chest_trap");
-        WoodBlockVariants.CHEST = new WoodBlockVariant("chest");
-        WoodBlockVariants.LOOM = new WoodBlockVariant("loom");
-        WoodBlockVariants.BARREL = new WoodBlockVariant("barrel");
+        LOG = new WoodBlockVariant("log");
+        STRIPPED_LOG = new WoodBlockVariant("stripped_log");
+        WOOD = new WoodBlockVariant("wood");
+        STRIPPED_WOOD = new WoodBlockVariant("stripped_wood");
+        LEAVES = new WoodBlockVariant("leaves");
+        PLANKS = new WoodBlockVariant("planks");
+        SAPLING = new WoodBlockVariant("sapling");
+        POTTED_SAPLING = new WoodBlockVariant("potted_sapling");
+        BOOKSHELF = new WoodBlockVariant("bookshelf");
+        DOOR = new WoodBlockVariant("door");
+        TRAPDOOR = new WoodBlockVariant("trapdoor");
+        FENCE = new WoodBlockVariant("fence");
+        LOG_FENCE = new WoodBlockVariant("log_fence");
+        FENCE_GATE = new WoodBlockVariant("fence_gate");
+        BUTTON = new WoodBlockVariant("button");
+        PRESSURE_PLATE = new WoodBlockVariant("pressure_plate");
+        SLAB_DOUBLE = new WoodBlockVariant("slab_double");
+        SLAB = new WoodBlockVariant("slab");
+        STAIRS = new WoodBlockVariant("stairs");
+        TOOL_RACK = new WoodBlockVariant("tool_rack");
+        SUPPORT = new WoodBlockVariant("support");
+        WORKBENCH = new WoodBlockVariant("workbench");
+        CHEST_TRAP = new WoodBlockVariant("chest_trap");
+        CHEST = new WoodBlockVariant("chest");
+        LOOM = new WoodBlockVariant("loom");
+        BARREL = new WoodBlockVariant("barrel");
     }
 }

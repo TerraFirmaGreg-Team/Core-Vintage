a35afa6bde502a87f377690ef0a5cbcb56146e2f
Xikaro <0regon.end@gmail.com>
2023-08-13 14:46:24 +0500

    полная документация для Soil

--- src/main/java/net/dries007/tfc/api/types/soil/ISoilBlock.java
+++ src/main/java/net/dries007/tfc/api/types/soil/ISoilBlock.java
@@ -10,23 +10,51 @@ import javax.annotation.Nonnull;
 
 import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
 
+/**
+ * Интерфейс, представляющий блок почвы.
+ */
 public interface ISoilBlock extends IHasModel, IItemProvider {
+    /**
+     * Возвращает вариант блока почвы.
+     *
+     * @return Вариант блока почвы.
+     */
     @Nonnull
     SoilBlockVariant getSoilBlockVariant();
 
+    /**
+     * Возвращает тип почвы.
+     *
+     * @return Тип почвы.
+     */
     @Nonnull
     SoilType getSoilType();
 
+    /**
+     * Возвращает местоположение регистрации блока почвы.
+     *
+     * @return Местоположение регистрации блока почвы.
+     */
     @Nonnull
     default ResourceLocation getRegistryLocation() {
         return new ResourceLocation(MOD_ID, String.format("soil/%s/%s", getSoilBlockVariant(), getSoilType()));
     }
 
+    /**
+     * Возвращает местоположение ресурса блока почвы.
+     *
+     * @return Местоположение ресурса блока почвы.
+     */
     @Nonnull
     default ResourceLocation getResourceLocation() {
         return new ResourceLocation(MOD_ID, String.format("soil/%s", getSoilBlockVariant()));
     }
 
+    /**
+     * Возвращает имя перевода блока почвы.
+     *
+     * @return Имя перевода блока почвы.
+     */
     @Nonnull
     default String getTranslationName() {
         return getRegistryLocation().toString().toLowerCase().replace(":", ".").replace("/", ".");
--- src/main/java/net/dries007/tfc/api/types/soil/variant/SoilBlockVariant.java
+++ src/main/java/net/dries007/tfc/api/types/soil/variant/SoilBlockVariant.java
@@ -10,6 +10,11 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.function.BiFunction;
 
+import static net.dries007.tfc.api.types.soil.variant.SoilBlockVariants.*;
+
+/**
+ * Класс, представляющий вариант блока почвы.
+ */
 public class SoilBlockVariant {
 
     private static final Set<SoilBlockVariant> SOIL_BLOCK_VARIANTS = new LinkedHashSet<>();
@@ -21,65 +26,114 @@ public class SoilBlockVariant {
     @Nullable
     private final FallingBlockManager.Specification fallingSpecification;
 
+    /**
+     * Создает экземпляр класса SoilBlockVariant с указанными параметрами.
+     *
+     * @param name                 Имя варианта блока почвы.
+     * @param factory              Фабричная функция для создания блока почвы.
+     * @param fallingSpecification Спецификация падающего блока (может быть null).
+     */
     public SoilBlockVariant(@Nonnull String name, @Nonnull BiFunction<SoilBlockVariant, SoilType, ISoilBlock> factory, @Nullable FallingBlockManager.Specification fallingSpecification) {
         this.name = name;
         this.factory = factory;
         this.fallingSpecification = fallingSpecification;
 
         if (name.isEmpty()) {
-            throw new RuntimeException(String.format("Rock name must contain any character: [%s]", name));
+            throw new RuntimeException(String.format("SoilBlockVariant name must contain any character: [%s]", name));
         }
 
         if (!SOIL_BLOCK_VARIANTS.add(this)) {
-            throw new RuntimeException(String.format("Rock: [%s] already exists!", name));
+            throw new RuntimeException(String.format("SoilBlockVariant: [%s] already exists!", name));
         }
     }
 
+    /**
+     * Возвращает набор всех вариантов блока почвы.
+     *
+     * @return Набор всех вариантов блока почвы.
+     */
     public static Set<SoilBlockVariant> getSoilBlockVariants() {
         return SOIL_BLOCK_VARIANTS;
     }
 
+    /**
+     * Возвращает строковое представление варианта блока почвы.
+     *
+     * @return Строковое представление варианта блока почвы.
+     */
     @Nonnull
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Возвращает спецификацию падающего блока.
+     *
+     * @return Спецификация падающего блока (может быть null).
+     */
     @Nullable
     public FallingBlockManager.Specification getFallingSpecification() {
         return fallingSpecification;
     }
 
+    /**
+     * Проверяет, может ли блок почвы падать.
+     *
+     * @return true, если блок почвы может падать, иначе false.
+     */
     public boolean canFall() {
         return fallingSpecification != null;
     }
 
+    /**
+     * Возвращает вариант блока почвы без травы.
+     *
+     * @return Вариант блока почвы без травы.
+     */
     public SoilBlockVariant getNonGrassVersion() {
-        if (this == SoilBlockVariants.GRASS || this == SoilBlockVariants.DRY_GRASS)
-            return SoilBlockVariants.DIRT;
+        if (this == GRASS || this == DRY_GRASS)
+            return DIRT;
 
-        if (this == SoilBlockVariants.CLAY_GRASS)
-            return SoilBlockVariants.CLAY;
+        if (this == CLAY_GRASS)
+            return CLAY;
 
         throw new IllegalStateException("Someone forgot to add enum constants to this switch case...");
     }
 
+    /**
+     * Возвращает вариант блока почвы с травой.
+     *
+     * @param spreader Вариант блока почвы, который распространяет траву.
+     * @return Вариант блока почвы с травой.
+     */
     public SoilBlockVariant getGrassVersion(SoilBlockVariant spreader) {
-        if (this == SoilBlockVariants.DIRT) {
-            return SoilBlockVariants.CLAY_GRASS;
+        if (this == DIRT) {
+            return CLAY_GRASS;
         }
 
-        if (this == SoilBlockVariants.CLAY) {
-            return spreader == SoilBlockVariants.DRY_GRASS ? SoilBlockVariants.DRY_GRASS : SoilBlockVariants.GRASS;
+        if (this == CLAY) {
+            return spreader == DRY_GRASS ? DRY_GRASS : GRASS;
         }
 
         throw new IllegalArgumentException(String.format("You cannot get grass from [%s] types.", spreader.toString()));
     }
 
+    /**
+     * Проверяет, является ли вариант блока почвы с травой.
+     *
+     * @return true, если вариант блока почвы с травой, иначе false.
+     */
     public boolean isGrass() {
-        return this == SoilBlockVariants.GRASS || this == SoilBlockVariants.DRY_GRASS || this == SoilBlockVariants.CLAY_GRASS;
+        return this == GRASS || this == DRY_GRASS || this == CLAY_GRASS;
     }
 
+    /**
+     * Применяет вариант блока почвы к фабрике для создания блока почвы.
+     *
+     * @param soilType Тип почвы.
+     * @return Блок почвы.
+     */
     @Nonnull
     public ISoilBlock applyToFactory(SoilType soilType) {
         return factory.apply(this, soilType);

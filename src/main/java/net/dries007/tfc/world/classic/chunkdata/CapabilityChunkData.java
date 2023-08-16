1d56aca5d9f34fd3838b3997422d6705cac2f1ca
Xikaro <0regon.end@gmail.com>
2023-08-16 06:45:16 +0500

    chunkdata документация

--- src/main/java/net/dries007/tfc/world/classic/chunkdata/CapabilityChunkData.java
+++ src/main/java/net/dries007/tfc/world/classic/chunkdata/CapabilityChunkData.java
@@ -14,33 +14,51 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
 
+/**
+ * Класс CapabilityChunkData содержит методы для работы с возможностью (capability) хранения данных чанка.
+ */
 @Mod.EventBusSubscriber(modid = MOD_ID)
 public final class CapabilityChunkData {
+    /**
+     * Ресурсное имя для возможности (capability) хранения данных чанка.
+     */
     public static final ResourceLocation CHUNK_DATA = new ResourceLocation(MOD_ID, "chunkdata");
 
+    /**
+     * Метод preInit() выполняет регистрацию возможности (capability) для хранения данных чанка.
+     */
     public static void preInit() {
+        // Регистрация возможности (capability) для хранения данных чанка
         CapabilityManager.INSTANCE.register(ChunkDataTFC.class, new ChunkDataTFC.ChunkDataStorage(), ChunkDataTFC::new);
     }
 
+    /**
+     * Метод onAttachCapabilitiesChunk() добавляет возможность (capability) для хранения данных чанка при присоединении возможностей к объекту Chunk.
+     */
     @SubscribeEvent
     public static void onAttachCapabilitiesChunk(AttachCapabilitiesEvent<Chunk> event) {
-        // Per #922, if there's no world or no world type, something is seriously violating our assumptions and we will just fail.
+        // Проверяем, что мир существует и его тип соответствует TerraFirmaCraft
+        // В противном случае, что-то нарушает наши предположения и мы просто завершаемся
         //noinspection ConstantConditions
         if (event.getObject().getWorld() != null && event.getObject().getWorld().getWorldType() == TerraFirmaCraft.WORLD_TYPE_TFC) {
+            // Добавляем возможность (capability) для хранения данных чанка
             event.addCapability(CHUNK_DATA, new ChunkDataProvider());
         }
     }
 
+    /**
+     * Метод onChunkWatchWatch() обрабатывает событие наблюдения за чанком и обновляет данные на стороне сервера и клиента.
+     */
     @SubscribeEvent
     public static void onChunkWatchWatch(ChunkWatchEvent.Watch event) {
         Chunk chunk = event.getChunkInstance();
         if (chunk != null) {
             ChunkDataTFC data = chunk.getCapability(ChunkDataProvider.CHUNK_DATA_CAPABILITY, null);
             if (data != null && data.isInitialized()) {
-                // Update server side climate
+                // Обновляем климат на стороне сервера
                 ClimateTFC.update(chunk.getPos(), data.getRegionalTemp(), data.getRainfall());
 
-                // Update client side data
+                // Обновляем данные на стороне клиента
                 NBTTagCompound nbt = (NBTTagCompound) ChunkDataProvider.CHUNK_DATA_CAPABILITY.writeNBT(data, null);
                 TerraFirmaCraft.NETWORK.sendTo(new PacketChunkData(chunk.getPos(), nbt, data.getRegionalTemp(), data.getRainfall()), event.getPlayer());
             }
--- src/main/java/net/dries007/tfc/world/classic/chunkdata/ChunkDataProvider.java
+++ src/main/java/net/dries007/tfc/world/classic/chunkdata/ChunkDataProvider.java
@@ -10,30 +10,47 @@ import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+/**
+ * Класс ChunkDataProvider реализует интерфейс ICapabilitySerializable для хранения данных чанка.
+ */
 public final class ChunkDataProvider implements ICapabilitySerializable<NBTTagCompound> {
+    /**
+     * Внедрение возможности (capability) для хранения данных чанка.
+     */
     @CapabilityInject(ChunkDataTFC.class)
     public static final Capability<ChunkDataTFC> CHUNK_DATA_CAPABILITY = Helpers.getNull();
 
+    /**
+     * Создание экземпляра класса ChunkDataTFC, который будет хранить данные чанка.
+     */
     private final ChunkDataTFC instance = CHUNK_DATA_CAPABILITY.getDefaultInstance();
 
     @Override
     public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
+        // Проверяем, имеет ли объект указанную возможность (capability).
+        // В данном случае, проверяем, является ли указанная возможность CHUNK_DATA_CAPABILITY.
         return capability == CHUNK_DATA_CAPABILITY;
     }
 
     @Nullable
     @Override
     public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
+        // Получаем указанную возможность (capability) для объекта.
+        // В данном случае, возвращаем экземпляр ChunkDataTFC, если указанная возможность равна CHUNK_DATA_CAPABILITY.
         return capability == CHUNK_DATA_CAPABILITY ? CHUNK_DATA_CAPABILITY.cast(instance) : null;
     }
 
     @Override
     public NBTTagCompound serializeNBT() {
+        // Сериализуем данные объекта в NBTTagCompound.
+        // Здесь мы используем возможность (capability) CHUNK_DATA_CAPABILITY для записи данных в NBTTagCompound.
         return (NBTTagCompound) CHUNK_DATA_CAPABILITY.writeNBT(instance, null);
     }
 
     @Override
     public void deserializeNBT(NBTTagCompound nbt) {
+        // Десериализуем данные из NBTTagCompound и применяем их к объекту.
+        // Здесь мы используем возможность (capability) CHUNK_DATA_CAPABILITY для чтения данных из NBTTagCompound.
         CHUNK_DATA_CAPABILITY.readNBT(instance, null, nbt);
     }
 }

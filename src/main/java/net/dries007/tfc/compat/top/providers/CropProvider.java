eedc5f492fb72f1b5a38c474eb7761fab01bbbb4
Xikaro <0regon.end@gmail.com>
2023-08-20 05:38:11 +0500

    CropProvider

--- src/main/java/net/dries007/tfc/compat/top/providers/CropProvider.java
+++ src/main/java/net/dries007/tfc/compat/top/providers/CropProvider.java
@@ -44,19 +44,6 @@ public class CropProvider implements IProbeInfoProvider {
                 iProbeInfo.text(new TextComponentTranslation("top.tfc.crop.not_growing").getFormattedText());
             }
 
-            int curStage = state.getValue(blockCropGrowing.getAgeProperty());
-            int maxStage = ((BlockCropGrowing) state.getBlock()).getMaxAge();
-
-            if (curStage == maxStage) {
-                iProbeInfo.text(new TextComponentTranslation("top.tfc.crop.growth", new TextComponentTranslation("top.tfc.crop.mature").getFormattedText()).getFormattedText());
-            } else {
-                float remainingTicksToGrow = Math.max(0, (type.getGrowthTicks() * (float) ConfigTFC.General.FOOD.cropGrowthTimeModifier) - te.getTicksSinceUpdate());
-                float curStagePerc = 1.0F - remainingTicksToGrow / type.getGrowthTicks();
-                // Don't show 100% since it still needs to check on randomTick to grow
-                float totalPerc = Math.min(0.99f, curStagePerc / maxStage + (float) curStage / maxStage) * 100;
-                String growth = String.format("%d%%", Math.round(totalPerc));
-                iProbeInfo.text(new TextComponentTranslation("top.tfc.crop.growth", growth).getFormattedText());
-            }
         } else if (state.getBlock() instanceof BlockCropDead) {
             iProbeInfo.text(new TextComponentTranslation("top.tfc.crop.dead_crop").getFormattedText());
         }

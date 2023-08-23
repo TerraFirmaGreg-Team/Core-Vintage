2450f2d8b8da1086f5af7c5587e7c67ffe01c1b7
Xikaro <0regon.end@gmail.com>
2023-08-24 00:13:54 +0500

    GuiCreateWorldMixin

--- src/main/java/net/dries007/tfc/mixins/minecraft/GuiCreateWorldMixin.java
+++ src/main/java/net/dries007/tfc/mixins/minecraft/GuiCreateWorldMixin.java
@@ -2,11 +2,14 @@ package net.dries007.tfc.mixins.minecraft;
 
 import net.dries007.tfc.TerraFirmaCraft;
 import net.minecraft.client.gui.GuiCreateWorld;
+import net.minecraft.world.WorldType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.client.gui.GuiScreen.isShiftKeyDown;
 
 @SuppressWarnings("all")
 @Mixin(value = GuiCreateWorld.class)
@@ -17,7 +20,11 @@ public class GuiCreateWorldMixin {
 
     @Inject(method = "canSelectCurWorldType", at = @At(value = "HEAD"), cancellable = true)
     private void onCanSelectCurWorldType(CallbackInfoReturnable<Boolean> cir) {
-        selectedIndex = TerraFirmaCraft.WORLD_TYPE_TFC.getId();
+        if (isShiftKeyDown()) {
+            selectedIndex = WorldType.DEBUG_ALL_BLOCK_STATES.getId();
+        } else {
+            selectedIndex = TerraFirmaCraft.WORLD_TYPE_TFC.getId();
+        }
         cir.setReturnValue(true);
     }
 }

0c1492be27ea1aaccaefae25e0cd33b4a651e157
Xikaro <0regon.end@gmail.com>
2023-08-26 04:43:44 +0500

    Update ISoilBlock.java

--- src/main/java/net/dries007/tfc/api/types/soil/ISoilBlock.java
+++ src/main/java/net/dries007/tfc/api/types/soil/ISoilBlock.java
@@ -12,50 +12,50 @@ import javax.annotation.Nonnull;
 /**
  * Интерфейс, представляющий блок почвы.
  */
-public interface ISoilBlock extends IHasModel, IItemProvider {
-    /**
-     * Возвращает вариант блока почвы.
-     *
-     * @return Вариант блока почвы.
-     */
-    @Nonnull
-    SoilBlockVariant getBlockVariant();
+public interface ISoilBlock extends IItemProvider, IHasModel {
+	/**
+	 * Возвращает вариант блока почвы.
+	 *
+	 * @return Вариант блока почвы.
+	 */
+	@Nonnull
+	SoilBlockVariant getBlockVariant();
 
-    /**
-     * Возвращает тип почвы.
-     *
-     * @return Тип почвы.
-     */
-    @Nonnull
-    SoilType getType();
+	/**
+	 * Возвращает тип почвы.
+	 *
+	 * @return Тип почвы.
+	 */
+	@Nonnull
+	SoilType getType();
 
-    /**
-     * Возвращает местоположение регистрации блока почвы.
-     *
-     * @return Местоположение регистрации блока почвы.
-     */
-    @Nonnull
-    default ResourceLocation getRegistryLocation() {
-        return TerraFirmaCraft.identifier(String.format("soil/%s/%s", getBlockVariant(), getType()));
-    }
+	/**
+	 * Возвращает местоположение регистрации блока почвы.
+	 *
+	 * @return Местоположение регистрации блока почвы.
+	 */
+	@Nonnull
+	default ResourceLocation getRegistryLocation() {
+		return TerraFirmaCraft.identifier(String.format("soil/%s/%s", getBlockVariant(), getType()));
+	}
 
-    /**
-     * Возвращает местоположение ресурса блока почвы.
-     *
-     * @return Местоположение ресурса блока почвы.
-     */
-    @Nonnull
-    default ResourceLocation getResourceLocation() {
-        return TerraFirmaCraft.identifier(String.format("soil/%s", getBlockVariant()));
-    }
+	/**
+	 * Возвращает местоположение ресурса блока почвы.
+	 *
+	 * @return Местоположение ресурса блока почвы.
+	 */
+	@Nonnull
+	default ResourceLocation getResourceLocation() {
+		return TerraFirmaCraft.identifier(String.format("soil/%s", getBlockVariant()));
+	}
 
-    /**
-     * Возвращает имя перевода блока почвы.
-     *
-     * @return Имя перевода блока почвы.
-     */
-    @Nonnull
-    default String getTranslationName() {
-        return getRegistryLocation().toString().toLowerCase().replace(":", ".").replace("/", ".");
-    }
+	/**
+	 * Возвращает имя перевода блока почвы.
+	 *
+	 * @return Имя перевода блока почвы.
+	 */
+	@Nonnull
+	default String getTranslationName() {
+		return getRegistryLocation().toString().toLowerCase().replace(":", ".").replace("/", ".");
+	}
 }

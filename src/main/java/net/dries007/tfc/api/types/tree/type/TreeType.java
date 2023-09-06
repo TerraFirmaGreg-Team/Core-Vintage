5b42eb5bec4ddd09b474594615a7c9e47055e8b1
Xikaro <0regon.end@gmail.com>
2023-09-06 11:33:59 +0500

    Update TreeType.java

--- src/main/java/net/dries007/tfc/api/types/tree/type/TreeType.java
+++ src/main/java/net/dries007/tfc/api/types/tree/type/TreeType.java
@@ -12,23 +12,15 @@ import net.dries007.tfc.util.calendar.ICalendar;
 import net.dries007.tfc.util.calendar.Month;
 import net.dries007.tfc.world.classic.worldgen.trees.ITreeGenerator;
 import net.dries007.tfc.world.classic.worldgen.trees.TreeGenDT;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.gen.structure.template.TemplateManager;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -322,17 +314,6 @@ public class TreeType {
 
     public ICellKit getCellKit() {
         return cellKit;
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void addInfo(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-        if (GuiScreen.isShiftKeyDown()) {
-            tooltip.add(TextFormatting.GRAY + I18n.format("tfc.tooltip.climate_info"));
-            tooltip.add(TextFormatting.BLUE + I18n.format("tfc.tooltip.climate_info_rainfall", (int) minRain, (int) maxRain));
-            tooltip.add(TextFormatting.GOLD + I18n.format("tfc.tooltip.climate_info_temperature", String.format("%.1f", minTemp), String.format("%.1f", maxTemp)));
-        } else {
-            tooltip.add(TextFormatting.GRAY + I18n.format("tfc.tooltip.hold_shift_for_climate_info"));
-        }
     }
 
     public static class Builder {

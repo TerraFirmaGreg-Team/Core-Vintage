01a5b3d9e24c9062309c8ea77c8867845c80ba72
Xikaro <0regon.end@gmail.com>
2023-09-14 19:30:16 +0500

    bush

--- src/main/java/net/dries007/tfc/api/types/crop/variant/item/CropItemVariantHandler.java
+++ src/main/java/net/dries007/tfc/api/types/crop/variant/item/CropItemVariantHandler.java
@@ -6,7 +6,6 @@ import static net.dries007.tfc.api.types.crop.variant.item.CropItemVariants.SEED
 
 public class CropItemVariantHandler {
     public static void init() {
-
         SEED = new CropItemVariant("seed", ItemCropSeed::new);
     }
 }

d29d4df19e3277dd7ce685e3320980b424d6282c
Xikaro <0regon.end@gmail.com>
2023-10-24 11:26:09 +0500

    horsepower

---
+++ src/main/java/se/gory_moon/horsepower/proxy/CommonProxy.java
@@ -0,0 +1,20 @@
+package se.gory_moon.horsepower.proxy;
+
+import se.gory_moon.horsepower.advancements.Manager;
+
+public class CommonProxy
+{
+    public void preInit()
+    {
+        Manager.register();
+    }
+
+    public void init()
+    {
+    }
+
+    public void loadComplete()
+    {
+
+    }
+}
---
+++ src/main/java/se/gory_moon/horsepower/tweaker/ITweakerPlugin.java
@@ -0,0 +1,12 @@
+package se.gory_moon.horsepower.tweaker;
+
+
+public interface ITweakerPlugin
+{
+
+    void applyTweaker();
+
+    void register();
+
+    void run();
+}

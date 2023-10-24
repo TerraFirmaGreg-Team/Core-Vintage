c02cfe83199f0e75cd1a66323ed56d688235ad0b
Xikaro <0regon.end@gmail.com>
2023-10-24 13:49:53 +0500

    TowerHeat

---
+++ src/main/java/BananaFructa/TowerHeat/Utils.java
@@ -0,0 +1,16 @@
+package BananaFructa.TowerHeat;
+
+import java.lang.reflect.Field;
+
+public class Utils {
+    public static <T> T readDeclaredField(Class<?> targetType, Object target, String name) {
+        try {
+            Field f = targetType.getDeclaredField(name);
+            f.setAccessible(true);
+            return (T) f.get(target);
+        } catch (Exception err) {
+            err.printStackTrace();
+            return null;
+        }
+    }
+}

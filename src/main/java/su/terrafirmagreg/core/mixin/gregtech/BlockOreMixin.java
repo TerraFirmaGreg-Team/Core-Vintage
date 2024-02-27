81c2aaf41fdf0e4a0bb6b2372456d494b1a994d2
Xikaro <0regon.end@gmail.com>
2024-02-27 09:48:23 +0500

    #237

--- src/main/java/su/terrafirmagreg/core/mixin/gregtech/BlockOreMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixin/gregtech/BlockOreMixin.java
@@ -1,41 +1,37 @@
 package su.terrafirmagreg.core.mixin.gregtech;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
-
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
 import gregtech.common.blocks.BlockOre;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import su.terrafirmagreg.core.TerraFirmaGreg;
 import su.terrafirmagreg.core.modules.gregtech.oreprefix.TFGOrePrefix;
 
 import java.util.Random;
 
-@Mixin(value = BlockOre.class)
+@Mixin(value = BlockOre.class, remap = false)
 public class BlockOreMixin {
 
-    @Unique
-    @Final
-    public Material field_149764_J;
+	@Shadow
+	@Final
+	public Material field_149764_J;
 
-    @Inject(method = "getItemDropped", at = @At(value = "HEAD"), cancellable = true)
-    private void getItemDropped(@NotNull IBlockState state, @NotNull Random rand, int fortune, CallbackInfoReturnable<Item> cir) {
-        var itemStack = OreDictUnifier.get(TFGOrePrefix.oreChunk, field_149764_J);
-        TerraFirmaGreg.LOGGER.info("ItemStack: " + itemStack.toString());
-        cir.setReturnValue(itemStack.getItem());
-    }
+	@Inject(method = "getItemDropped", at = @At(value = "HEAD"), remap = false, cancellable = true)
+	private void getItemDropped(@NotNull IBlockState state, @NotNull Random rand, int fortune, CallbackInfoReturnable<Item> cir) {
+		var itemStack = OreDictUnifier.get(TFGOrePrefix.oreChunk, field_149764_J);
+		cir.setReturnValue(itemStack.getItem());
+	}
 
-    @Inject(method = "damageDropped", at = @At(value = "HEAD"), cancellable = true)
-    private void damageDropped(IBlockState state, CallbackInfoReturnable<Integer> cir) {
-        var itemStack = OreDictUnifier.get(TFGOrePrefix.oreChunk, field_149764_J);
-        TerraFirmaGreg.LOGGER.info("ItemStack: " + itemStack.toString());
-        cir.setReturnValue(itemStack.getItemDamage());
-    }
+	@Inject(method = "damageDropped", at = @At(value = "HEAD"), remap = false, cancellable = true)
+	private void damageDropped(IBlockState state, CallbackInfoReturnable<Integer> cir) {
+		var itemStack = OreDictUnifier.get(TFGOrePrefix.oreChunk, field_149764_J);
+		cir.setReturnValue(itemStack.getItemDamage());
+	}
 }
--- src/main/java/su/terrafirmagreg/core/modules/gregtech/stonetypes/StoneTypeHandler.java
+++ src/main/java/su/terrafirmagreg/core/modules/gregtech/stonetypes/StoneTypeHandler.java
@@ -1,11 +1,11 @@
 package su.terrafirmagreg.core.modules.gregtech.stonetypes;
 
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.ore.OrePrefix;
 import gregtech.api.unification.ore.StoneType;
+import net.minecraft.block.SoundType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import su.terrafirmagreg.core.util.Triple;
 
 import java.util.LinkedHashSet;
@@ -18,55 +18,55 @@ import static su.terrafirmagreg.core.modules.gregtech.oreprefix.TFGOrePrefix.*;
 
 public class StoneTypeHandler {
 
-    @SuppressWarnings("ConstantConditions")
-    public static void init() {
-        final Set<Triple<String, OrePrefix, Material>> rockTypes = new LinkedHashSet<>() {
-            {
-                add(new Triple<>("andesite", oreAndesite, Andesite));
-                add(new Triple<>("basalt", oreBasalt, Basalt));
-                add(new Triple<>("breccia", oreBreccia, Breccia));
-                add(new Triple<>("catlinite", oreCatlinite, Catlinite));
-                add(new Triple<>("chalk", oreChalk, Chalk));
-                add(new Triple<>("chert", oreChert, Chert));
-                add(new Triple<>("claystone", oreClaystone, Claystone));
-                add(new Triple<>("conglomerate", oreConglomerate, Conglomerate));
-                add(new Triple<>("dacite", oreDacite, Dacite));
-                add(new Triple<>("diorite", oreDiorite, Diorite));
-                add(new Triple<>("dolomite", oreDolomite, Dolomite));
-                add(new Triple<>("gabbro", oreGabbro, Gabbro));
-                add(new Triple<>("gneiss", oreGneiss, Gneiss));
-                add(new Triple<>("granite", oreGranite, Granite));
-                add(new Triple<>("komatiite", oreKomatiite, Komatiite));
-                add(new Triple<>("limestone", oreLimestone, Limestone));
-                add(new Triple<>("marble", oreMarble, Marble));
-                add(new Triple<>("mudstone", oreMudstone, Mudstone));
-                add(new Triple<>("novaculite", oreNovaculite, Novaculite));
-                add(new Triple<>("peridotite", orePeridotite, Peridotite));
-                add(new Triple<>("porphyry", orePorphyry, Porphyry));
-                add(new Triple<>("quartzite", oreQuartzite, Quartzite));
-                add(new Triple<>("rhyolite", oreRhyolite, Rhyolite));
-                add(new Triple<>("rocksalt", oreRockSalt, RockSalt));
-                add(new Triple<>("sandstone", oreSandstone, Sandstone));
-                add(new Triple<>("schist", oreSchist, Schist));
-                add(new Triple<>("shale", oreShale, Shale));
-                add(new Triple<>("siltstone", oreSiltstone, Siltstone));
-                add(new Triple<>("slate", oreSlate, Slate));
-                add(new Triple<>("soapstone", oreSoapstone, Soapstone));
-                add(new Triple<>("phyllite", orePhyllite, Phyllite));
-            }
-        };
+	@SuppressWarnings("ConstantConditions")
+	public static void init() {
+		final Set<Triple<String, OrePrefix, Material>> rockTypes = new LinkedHashSet<>() {
+			{
+				add(new Triple<>("andesite", oreAndesite, Andesite));
+				add(new Triple<>("basalt", oreBasalt, Basalt));
+				add(new Triple<>("breccia", oreBreccia, Breccia));
+				add(new Triple<>("catlinite", oreCatlinite, Catlinite));
+				add(new Triple<>("chalk", oreChalk, Chalk));
+				add(new Triple<>("chert", oreChert, Chert));
+				add(new Triple<>("claystone", oreClaystone, Claystone));
+				add(new Triple<>("conglomerate", oreConglomerate, Conglomerate));
+				add(new Triple<>("dacite", oreDacite, Dacite));
+				add(new Triple<>("diorite", oreDiorite, Diorite));
+				add(new Triple<>("dolomite", oreDolomite, Dolomite));
+				add(new Triple<>("gabbro", oreGabbro, Gabbro));
+				add(new Triple<>("gneiss", oreGneiss, Gneiss));
+				add(new Triple<>("granite", oreGranite, Granite));
+				add(new Triple<>("komatiite", oreKomatiite, Komatiite));
+				add(new Triple<>("limestone", oreLimestone, Limestone));
+				add(new Triple<>("marble", oreMarble, Marble));
+				add(new Triple<>("mudstone", oreMudstone, Mudstone));
+				add(new Triple<>("novaculite", oreNovaculite, Novaculite));
+				add(new Triple<>("peridotite", orePeridotite, Peridotite));
+				add(new Triple<>("porphyry", orePorphyry, Porphyry));
+				add(new Triple<>("quartzite", oreQuartzite, Quartzite));
+				add(new Triple<>("rhyolite", oreRhyolite, Rhyolite));
+				add(new Triple<>("rocksalt", oreRockSalt, RockSalt));
+				add(new Triple<>("sandstone", oreSandstone, Sandstone));
+				add(new Triple<>("schist", oreSchist, Schist));
+				add(new Triple<>("shale", oreShale, Shale));
+				add(new Triple<>("siltstone", oreSiltstone, Siltstone));
+				add(new Triple<>("slate", oreSlate, Slate));
+				add(new Triple<>("soapstone", oreSoapstone, Soapstone));
+				add(new Triple<>("phyllite", orePhyllite, Phyllite));
+			}
+		};
 
-        int counter = 16;
+		int counter = 16;
 
-        for (Triple<String, OrePrefix, Material> triple : rockTypes) {
-            new StoneType(
-                    counter, "tfc_" + triple.getLeft(), net.minecraft.block.SoundType.STONE, triple.getMiddle(), triple.getRight(),
-                    () -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation("tfc:raw/" + triple.getLeft())).getDefaultState(),
-                    state -> state.getBlock() == ForgeRegistries.BLOCKS.getValue(new ResourceLocation("tfc:raw/" + triple.getLeft())),
-                    false
-            );
+		for (Triple<String, OrePrefix, Material> triple : rockTypes) {
+			new StoneType(
+					counter, "tfc_" + triple.getLeft(), SoundType.STONE, triple.getMiddle(), triple.getRight(),
+					() -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation("tfc:raw/" + triple.getLeft())).getDefaultState(),
+					state -> state.getBlock() == ForgeRegistries.BLOCKS.getValue(new ResourceLocation("tfc:raw/" + triple.getLeft())),
+					false
+			);
 
-            counter++;
-        }
-    }
+			counter++;
+		}
+	}
 }

e30111ba67aa87fcaa063f764dcee2a14bfecc91
Dmitry <ondyshev.dmitry@gmail.com>
2024-08-26 09:54:12 +0700

    fix: some items cant be processed inside gt machines because CAPABILITY

---
+++ src/main/java/su/terrafirmagreg/core/mixin/gregtech/GTRecipeItemInputMixin.java
@@ -0,0 +1,20 @@
+package su.terrafirmagreg.core.mixin.gregtech;
+
+import gregtech.api.recipes.ingredients.GTRecipeItemInput;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = GTRecipeItemInput.class, remap = false)
+public class GTRecipeItemInputMixin {
+
+    /**
+     * Исправляет проблему, когда некоторые рецепты содержащие капабилити нагрева не перерабатываются в рецептах.
+     * Возможно стоит добавить возврат true, только если у предмета есть капабилити нагрева? Стоит подумать.
+     * */
+    @Redirect(method = "acceptsStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;areCapsCompatible(L;)Z"), remap = false)
+    private boolean onAreCapsCompatible(ItemStack stack) {
+        return true;
+    }
+
+}
